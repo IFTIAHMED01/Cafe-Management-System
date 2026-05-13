955216b289710d3c42c2bed40a5e64d7426de6d8
IFTI_AHMED <iftiahmed1997@gmail.com>
2026-05-13 18:01:28 +0600

    Making some changes

--- #Creating a Cafe management system.py
+++ #Creating a Cafe management system.py
@@ -17,17 +17,19 @@ Total_Price = 0
 
 Answer = input("Are you ready for  order? (Yes/No)")
 
-item_1 = input("Enter the name of item you want to order = ")
-if item_1 in menu:
+if Answer == "Yes":
+
+ item_1 = input("Enter the name of item you want to order = ")
+ if item_1 in menu:
     Total_Price+= menu[item_1]
     print(f"Your item {item_1} has been added to your order")
 
-else:
+ else:
     print(f"Ordered item {item_1} is not available yet!")
 
 
-anotheritem = input("Do you want to add another item? (Yes/No)")
-if anotheritem == "Yes":
+ anotheritem = input("Do you want to add another item? (Yes/No)")
+ if anotheritem == "Yes":
     item_2 = input("Enter the name of item you want to order =")
     if item_2 in menu:
         Total_Price+= menu[item_2]
@@ -36,7 +38,9 @@ if anotheritem == "Yes":
     else:
         print(f"Ordered item {item_1} is not available yet!")
 
-print(f"The amount of items to pay is BDT {Total_Price}")
+ print(f"The amount of items to pay is BDT {Total_Price}")
+
+print("Thank you for coming to the Restaurant")
 
 
 
