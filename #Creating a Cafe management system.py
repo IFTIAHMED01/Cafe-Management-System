26f889788d2543674b764c7fe7c766e41c6293b9
IFTI_AHMED <iftiahmed1997@gmail.com>
2026-05-13 06:03:20 +0600

    Creating a Cafe Management System (Initial Python Code)

---
+++ #Creating a Cafe management system.py
@@ -0,0 +1,41 @@
+#Creating a Cafe management system
+
+menu = {
+    'Chicken Breast': 40,
+    'Chicken Wings': 20,
+    'Beef/ Chicken Burger': 60,
+    'Brownie': 15,
+    'Coffee': 10
+}
+
+#Printing the menu
+print("Welcome to Barcode Restaurant")
+print("Chicken Breast: BDT 40\nChicken Wings: BDT 20\nBeef/Chicken Burger: BDT 60\nBrownie: BDT 15\nCoffee: BDT 10" )
+
+Total_Price = 0
+
+item_1 = input("Enter the name of item you want to order = ")
+if item_1 in menu:
+    Total_Price+= menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
+
+anotheritem = input("Do you want to add another item? (Yes/No)")
+if anotheritem == "Yes":
+    item_2 = input("Enter the name of item you want to order =")
+    if item_2 in menu:
+        Total_Price+= menu[item_2]
+        print(f"Your item {item_1} has been added to your order")
+    
+    else:
+        print(f"Ordered item {item_1} is not available yet!")
+
+print(f"The amount of items to pay is BDT {Total_Price}")
+
+
+
+
+
