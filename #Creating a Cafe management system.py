f321e2e9a0104b069e01b2415894ba811d904006
IFTI_AHMED <iftiahmed1997@gmail.com>
2026-05-13 18:20:07 +0600

    Final source of code

--- #Creating a Cafe management system.py
+++ #Creating a Cafe management system.py
@@ -15,30 +15,33 @@ print("Chicken Breast: BDT 40\nChicken Wings: BDT 20\nBeef/Chicken Burger: BDT 6
 
 Total_Price = 0
 
-Answer = input("Are you ready for  order? (Yes/No)")
+Answer = input("Are you ready for order? (Yes/No)")
 
 if Answer == "Yes":
-
- item_1 = input("Enter the name of item you want to order = ")
- if item_1 in menu:
+ while True:
+  item_1 = input("Enter the name of item you want to order = ")
+  if item_1 in menu:
     Total_Price+= menu[item_1]
     print(f"Your item {item_1} has been added to your order")
 
- else:
+  else:
     print(f"Ordered item {item_1} is not available yet!")
 
 
- anotheritem = input("Do you want to add another item? (Yes/No)")
- if anotheritem == "Yes":
+  anotheritem = input("Do you want to add another item? (Yes/No)")
+  if anotheritem == "Yes":
     item_2 = input("Enter the name of item you want to order =")
     if item_2 in menu:
         Total_Price+= menu[item_2]
-        print(f"Your item {item_1} has been added to your order")
+        print(f"Your item {item_1} has been added to your order") 
     
     else:
         print(f"Ordered item {item_1} is not available yet!")
+  answer = input("Do you want to order anything else! (Yes/No)")
+  if answer == "No":
+    break
 
- print(f"The amount of items to pay is BDT {Total_Price}")
+print(f"The amount of items to pay is BDT {Total_Price}")
 
 print("Thank you for coming to the Restaurant")
 
